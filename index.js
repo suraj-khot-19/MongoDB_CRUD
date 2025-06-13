f19b3ee3450a283b9ee3fd6bdcfe34d69d2a0fcc
suraj-khot-19 <surajkhot19082001@gmail.com>
2025-06-13 16:10:17 +0530

    npm i express > simple get request for /

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("Hi there...")
+const express = require("express")
+
+const app = express();
+
+const port = 3000;
+
+app.listen(port)
+
+app.get("/", (req, res) => {
+    res.send({
+        msg:"app is listinig to port 3000"
+    })
+});
+
